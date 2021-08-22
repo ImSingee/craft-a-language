1b6970edd39ab87ad9cc468ced2c3aeaef414d4d
Singee <git@singee.me>
2021-08-22 17:38:25 +0800

    l01: 修复未处理嵌套函数的问题

--- l01/src/ref_resolver.rs
+++ l01/src/ref_resolver.rs
@@ -1,5 +1,6 @@
 use crate::grammar::{FunctionDecl, Statement};
 use crate::prog::Prog;
+use crate::FunctionCall;
 use std::collections::HashMap;
 
 pub struct RefResolver {}
@@ -8,20 +9,40 @@ impl RefResolver {
         let mut functions: HashMap<String, std::ptr::NonNull<FunctionDecl>> = HashMap::new();
 
         for x in &mut prog.stmts {
+            if let Statement::FunctionDecl(decl) = x {
+                functions.insert(decl.name.to_string(), decl.into());
+            }
+        }
+
+        for x in &mut prog.stmts {
             match x {
                 Statement::FunctionDecl(decl) => {
-                    functions.insert(decl.name.to_string(), decl.into());
+                    for call in &mut decl.body.stmts {
+                        RefResolver::resolve_function_call(&functions, call)?
+                    }
                 }
-                Statement::FunctionCall(call) => match functions.get(&call.name) {
-                    None => match call.name.as_ref() {
-                        "println" => {}
-                        _ => return Err(format!("unkown name {}", call.name)),
-                    },
-                    Some(ptr) => call.definition = Some(ptr.clone()),
-                },
+                Statement::FunctionCall(call) => {
+                    RefResolver::resolve_function_call(&functions, call)?
+                }
             }
         }
 
         Ok(())
+    }
+
+    fn resolve_function_call(
+        functions: &HashMap<String, std::ptr::NonNull<FunctionDecl>>,
+        call: &mut FunctionCall,
+    ) -> Result<(), String> {
+        match functions.get(&call.name) {
+            None => match call.name.as_ref() {
+                "println" => Ok(()),
+                _ => Err(format!("unkown function {}", call.name)),
+            },
+            Some(ptr) => {
+                call.definition = Some(ptr.clone());
+                Ok(())
+            }
+        }
     }
 }
